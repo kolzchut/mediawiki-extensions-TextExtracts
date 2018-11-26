ad504b7a3c0d2664b8ac826aa22ea51b3a7235c8
Dror S. [ffs] <FreedomFighterSparrow@gmail.com>
2018-11-26 04:30:08 +0200

    Change Cargo table name to page_synopsis and remove extra column

--- includes/textExtractOverride.php
+++ includes/textExtractOverride.php
@@ -14,7 +14,12 @@ function TextExtractOverrideParseByTitle( $title, $alterntiveText ) {
 }	
 
 function TextExtractOverrideGetDataFromCargo( $name, $title ) {
-	$sqlQuery = \CargoSQLQuery::newFromValues( 'meta_data', $name, "meta_data._pageName='{$title->mTextform}'",'','','','','');
+	$sqlQuery = \CargoSQLQuery::newFromValues(
+		'page_synopsis',
+		$name,
+		"page_synopsis._pageName='{$title->mTextform}'",
+		'', '', '', '', '', ''
+	);
 	try {
 		$queryResults = $sqlQuery->run();
 
@@ -24,34 +29,27 @@ function TextExtractOverrideGetDataFromCargo( $name, $title ) {
 	return $queryResults;
 }
 function TextExtractOverrideGetDescriptionFromCargo( $title ) {
-		//die(print_r($title));
-		if(class_exists('CargoSQLQuery')){
-			$queryResults = TextExtractOverrideGetDataFromCargo('excerpt', $title);
+		if ( class_exists( 'CargoSQLQuery' ) ){
+			$queryResults = TextExtractOverrideGetDataFromCargo( 'excerpt', $title );
 
-			if(!count($queryResults) || !$queryResults[0]['excerpt']){
-				$queryResults = TextExtractOverrideGetDataFromCargo('excerpt-text', $title);
-			}
-			//die(print_r(['mmm',$queryResults[0]]));
 			$formattedData = null;
 			foreach ( $queryResults as $row ) {
-				if($row){
-					$textFromRow = isset($row['excerpt']) ? $row['excerpt'] : $row['excerpt-text'];
+				if ( $row ){
+					$textFromRow = $row['excerpt'];
 					$myParse = new \Parser();
-					$formattedData = $myParse->parse($textFromRow, $title, new \ParserOptions());
-					$text = html_entity_decode($formattedData->getText());
+					$formattedData = $myParse->parse( $textFromRow, $title, new \ParserOptions() );
+					$text = html_entity_decode( $formattedData->getText() );
 					$stripper = new StripStateOverride;
 					$regex = $stripper->getRegex();
-					$regex = preg_replace('/\x7f/', '\?', $regex);
-					//echo 'fdsdsfdf' . $regex;
-					$formattedData = preg_replace($regex, '', $text);
-					//die('bbb' . $formattedData);
+					$regex = preg_replace( '/\x7f/', '\?', $regex );
+					$formattedData = preg_replace( $regex, '', $text );
 					break;
 				}
-				
 			}
-			//die(print_r($formattedData));
 			$text_to_return = $formattedData ? $formattedData . '...' : '';
 
-			return strip_tags($text_to_return);
+			return strip_tags( $text_to_return );
 		}
+
+		return null;
 	}
